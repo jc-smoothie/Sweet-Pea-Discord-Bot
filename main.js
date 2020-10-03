15d12af91709b6457f8c4037786b40fafd6df13e
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-03 17:50:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -29,15 +29,21 @@ client.once('ready', () => {
 client.on('message', message => {
     const reply = message.content
     if(reply == 'cool'){
-        message.channel.send("nice");
+        setTimeout(function(){
+            message.channel.send("nice");
+        }, 1000);
     } else if(reply == 'introduce yourself'){
-        message.channel.send("My name is Sewwt Pea, and I am a discord bot coded in JavaScript by you, jc smoothie!");
+        setTimeout(function(){
+            message.channel.send("My name is Sweet Pea, and I am a discord bot coded in JavaScript by you, jc smoothie!");
+        }, 1000);
         setTimeout(function(){
             message.channel.send("I can help members in the server self-assign roles, and role a dice.");
-        }, 2000);
+        }, 1000);
         //message.channel.send("I can help members in the server self-assign roles, and role a dice.");
     } else if(reply == "okay, that's all"){
-        message.channel.send("Sweet, bye!~");
+        setTimeout(function(){
+            message.channel.send("Sweet, bye!~");
+        }, 1000);
     }
 });
 
