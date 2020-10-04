c9aa64ed0e5671eff6bd9a2b07e00d4fb9e26581
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-04 00:19:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -85,35 +85,6 @@ client.on('message', message => {
        message.reply("You rolled a " + dice + "!");
        //console.log(message.guild.roles);
    } else if(command == 'hug'){
-       client.on('message', message => {
-           let urls = [
-               'https://cdn.discordapp.com/attachments/730136973372555386/733655951374024775/giphy.gif',
-               'https://cdn.discordapp.com/attachments/730136973372555386/733655954704564295/giphy-1.gif',
-               'https://cdn.discordapp.com/attachments/730136973372555386/733655971049767032/nrdYNtL.gif',
-               'https://cdn.discordapp.com/attachments/730136973372555386/733655977542287390/anime-hug.gif',
-               'https://cdn.discordapp.com/attachments/730136973372555386/733655981442990169/tumblr_ma7l17EWnk1rq65rlo1_500.gif',
-               'https://cdn.discordapp.com/attachments/730136973372555386/733655982890156032/b62f047f8ed11b832cb6c0d8ec30687b.gif',
-               'https://cdn.discordapp.com/attachments/730136973372555386/733656000049053766/1451239600645.gif',
-               'https://cdn.discordapp.com/attachments/730136973372555386/733655996781690890/tumblr_939249beed1e6e81801ac8391bb8a203_111e1ef2_500.gif',
-               'https://cdn.discordapp.com/attachments/730136973372555386/733655994063650906/tenor.gif',
-               'https://cdn.discordapp.com/attachments/730136973372555386/733655987331924008/c61.gif'
-           ];
-
-           const hugging = message.content.split(' ');
-           var personTagged = hugging[1];
-           
-           const image = urls[Math.floor(Math.random() * urls.length/10)]
-           //var dice_number = parseInt(dice_result, 10);
-           //var dice = Math.floor((Math.random() * dice_number));
-           
-           const hugEmbed = new MessageEmbed()
-           .setTitle("Hugs")
-           .setDescription(message.author.username + ' hugs ' + personTagged)
-           .setImage(image)
-           .setColor("RANDOM")
-           message.channel.send(hugEmbed);
-       });
-   } else if(command == 'hug2'){
        client.commands.get('hug').execute(message, args);
    } else if(command == 'invite'){
        const invite = new MessageEmbed()
