fb4e35a3dba7e3f399d99e4cab9293363861c6fa
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-07 17:54:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -140,7 +140,8 @@ client.on("messageReactionAdd", async (reaction, user) => {
     if(reaction.message.channel.id === "763441746314133505"){
         if(reaction.emoji.name ==='✅'){
             reaction.message.guild.members.cache.get(user.id).roles.add(Test)
-            reaction.message.reply(763519137821098045 + "You now have the role!")
+            reaction.message.reply("You now have the role!")
+            //763519137821098045
         }
     }
 })
@@ -155,7 +156,8 @@ client.on("messageReactionRemove", async (reaction, user) => {
     if(reaction.message.channel.id === "763441746314133505"){
         if(reaction.emoji.name ==='✅'){
             reaction.message.guild.members.cache.get(user.id).roles.remove(Test)
-            reaction.message.reply(763519159753244702 + "Your role has been removed.")
+            reaction.message.reply("Your role has been removed.")
+            //763519159753244702
         }
     }
 })
