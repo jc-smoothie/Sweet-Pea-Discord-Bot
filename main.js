3db94e45413b92567e8d19e6d5ede80bc2034b4a
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-07 11:39:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 
-//const client = new Discord.Client();
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 const {Client, MessageEmbed} = require('discord.js');
 
-const client = new Client();
+//const client = new Client();
 
 const prefix = '+';
 
