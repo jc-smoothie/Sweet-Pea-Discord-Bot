478fce5908b8a8cd1c9c14c95163f7b7ccc220cc
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-07 23:38:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,15 +15,15 @@ client.commands = new Discord.Collection();
 
 //Variables
 const Test = '763441410032140318';
-function makeCounter(){
-    let count = 0;
+//function makeCounter(){
+    //let count = 0;
     
-    return function(){
-        return count++;
-    }
-}
+    //return function(){
+        //return count++;
+    //}
+//}
 
-let counter = makeCounter();
+//let counter = makeCounter();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
@@ -138,15 +138,6 @@ client.on('message', message => {
            
            if(personTagged.displayName == "Sweet Pea"){
                message.reply("❤️ Aww, thanks! I feel better now.");
-               function makeCounter(){
-                   let count = 0;
-                   
-                   return function(){
-                       return count++;
-                   }
-               }
-              
-              let counter = makeCounter();
            }
        }
    } else if(command == 'slap'){
@@ -175,28 +166,25 @@ client.on('message', message => {
            //sentMessage.delete(5000);
        //});
    } else if(command == 'token'){
-       function makeCounter(){
-           let count = 0;
-           
-           return function(){
-               return count++;
-           };
-       }
+       var attempts = 1
        if(attempts == 1){
+           var attempts = 2
            message.reply("Sorry, but I can't reveal my token to anyone in a text channel.")
        } else if(attempts == 2){
+           var attempts = 3
            message.reply("Once again, I simply can't display my token in a text channel, otherwise others will see. \n Others can write code and commands, and control me with my token.")
        } else if(attempts == 3){
+           var attempts = 4
            message.reply("Now this is just bullying! I'm telling you, I can't reveal my token! ( >~<')")
        } else if(attempts == 4){
+           var attempts = 5   
            message.reply("Mo yada!!")
        } else if(attempts == 5){
+           var attempts = 6
        } else if(attempts == 6){
+           var attempts = 7
        } else if (attempts == 7){   
            message.reply("(;-;)")
-           function forgiven(){
-               var forgiven = 0
-           }
        }
 
        const reply = message.content
