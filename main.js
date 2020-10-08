dc1b400d51f21f0495108dec5a7de31c1debda58
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-07 22:36:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ client.commands = new Discord.Collection();
 
 //Variables
 const Test = '763441410032140318';
-var attempts = 0
+var forgiven = 1
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
@@ -91,7 +91,48 @@ client.on('message', message => {
        message.reply("You rolled a " + dice + "!");
        //console.log(message.guild.roles);
    } else if(command == 'hug'){
-       client.commands.get('hug').execute(message, args);
+       //client.commands.get('hug').execute(message, args);
+       if (!args[0]){
+           message.channel.send('Missing Arguments!')
+           console.log('Missing args')
+       } else{
+           console.log('should work.')
+           const image = Math.floor((Math.random() * 10) + 1);
+           if(image == 1){
+               var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733655951374024775/giphy.gif';
+           } else if(image == 2){
+               var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733655954704564295/giphy-1.gif';
+           } else if(image == 3){
+               var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733655971049767032/nrdYNtL.gif';
+           } else if(image == 4){
+               var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733655977542287390/anime-hug.gif';
+           } else if(image == 5){
+               var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733655981442990169/tumblr_ma7l17EWnk1rq65rlo1_500.gif';
+           } else if(image == 6){
+            var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733655982890156032/b62f047f8ed11b832cb6c0d8ec30687b.gif';
+           } else if(image == 7){
+               var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733656000049053766/1451239600645.gif';
+           } else if(image == 8){
+               var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733655996781690890/tumblr_939249beed1e6e81801ac8391bb8a203_111e1ef2_500.gif';
+           } else if(image == 9){
+               var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733655994063650906/tenor.gif';
+           } else if(image == 10){
+               var chosenImage = 'https://cdn.discordapp.com/attachments/730136973372555386/733655987331924008/c61.gif';
+           }
+           const personTagged = message.mentions.members.first();
+           
+           let embed = new Discord.MessageEmbed()
+           .setTitle("Hugs")
+           .setDescription(message.author.username + ' hugs ' + personTagged.displayName)
+           .setImage(chosenImage)
+           .setColor("RANDOM")
+           message.channel.send(embed)
+           
+           if(personTagged.displayName == "Sweet Pea"){
+               message.reply("❤️ Aww, thanks! I feel better now.");
+               var forgiven = 1
+           }
+       }
    } else if(command == 'slap'){
        client.commands.get('slap').execute(message, args);
    } else if(command == 'dog'){
@@ -118,21 +159,30 @@ client.on('message', message => {
            //sentMessage.delete(5000);
        //});
    } else if(command == 'token'){
-       var attempts = attempts + 1
+       var attempts = 1
        if(attempts == 1){
+           var attempts = 2
            message.reply("Sorry, but I can't reveal my token to anyone in a text channel.")
        } else if(attempts == 2){
+           var attempts = 3
            message.reply("Once again, I simply can't display my token in a text channel, otherwise others will see. \n Others can write code and commands, and control me with my token.")
        } else if(attempts == 3){
+           var attempts = 4
            message.reply("Now this is just bullying! I'm telling you, I can't reveal my token! ( >~<')")
        } else if(attempts == 4){
+           var attempts = 5
            message.reply("Mo yada!!")
-       } else if (attempts == 7){
+       } else if(attempts == 5){
+           var attempts = 6
+       } else if(attempts == 6){
+           var attempts = 7
+       } else if (attempts == 7){   
            message.reply("(;-;)")
+           var forgiven = 0
        }
 
        const reply = message.content
-       const replyFormatted = replt.toLowerCase();
+       const replyFormatted = reply.toLowerCase();
 
        if(!replyFormatted == "I'm sorry"){
            return;
