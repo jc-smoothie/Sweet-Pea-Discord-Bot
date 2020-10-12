f09f73b326d276181223d35b71625e954d269307
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-12 09:52:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -744,5 +744,5 @@ client.on("messageReactionRemove", async (reaction, user) => {
     }
 });
 
-client.login('NzI5MTQyMDczMTI2NjgyNjQ0.XwEoeQ.XYTNMFYBZtO7hHCEoTi2MvlsbZU');
+client.login('NzI5MTQyMDczMTI2NjgyNjQ0.XwEoeQ.FlVxxJeRN2mScT4vxGD8fY24AVo');
 //client.login('NzQyMTE3MDUxNzYxNjg4NjM3.XzBcXQ.8bnHz6YKfLAYO_Wlk1s-WxV-Gjw');
