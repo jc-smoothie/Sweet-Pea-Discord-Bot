0569157674547250a596e9bf2be8b23813f23d72
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-15 11:03:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -914,6 +914,10 @@ client.on("messageReactionAdd", async (reaction, user) => {
             if(start == 0){
                 king();
                 startAdd();
+                var msgSend = message.channel.send("Royal Advisor \n [My" + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
+                let msg = await message.channel.send(msgSend)
+                msg.react('✅')
+                msg.react('❌')
             }
         }
     }
@@ -932,6 +936,10 @@ client.on("messageReactionAdd", async (reaction, user) => {
             if(start == 0){
                 queen();
                 startAdd();
+                var msgSend = message.channel.send("Royal Advisor \n [My" + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
+                let msg = await message.channel.send(msgSend)
+                msg.react('✅')
+                msg.react('❌')
             }
         }
     }
