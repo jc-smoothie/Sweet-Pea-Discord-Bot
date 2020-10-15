41e21b0f8b4bf531bfd54f76063b64a3a05acd41
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-15 11:26:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -916,10 +916,15 @@ client.on("messageReactionAdd", async (reaction, user) => {
             if(start == 0){
                 king();
                 startAdd();
-                var msgSend = reaction.message.channel.send("Royal Advisor \n [My" + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
-                let msg = await reaction.message.channel.send(msgSend)
-                msg.react('✅')
-                msg.react('❌')
+                let reactionsEmbed = new MessageEmbed()
+                .setTitle('Royal Advisor')
+                .setDescription("[My " + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]")
+                .setColor('#66ccff')
+                let messageEmbed = await message.channel.send(reactionsEmbed)
+                //var msgSend = reaction.message.channel.send("Royal Advisor \n [My " + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
+                //let msg = await reaction.message.channel.send(msgSend)
+                messageEmbed.react('✅')
+                messageEmbed.react('❌')
             }
         }
     }
@@ -938,7 +943,7 @@ client.on("messageReactionAdd", async (reaction, user) => {
             if(start == 0){
                 queen();
                 startAdd();
-                var msgSend = reaction.message.channel.send("Royal Advisor \n [My" + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
+                var msgSend = reaction.message.channel.send("Royal Advisor \n [My " + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
                 let msg = await reaction.message.channel.send(msgSend)
                 msg.react('✅')
                 msg.react('❌')
