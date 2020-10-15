54f1c845fd68af4981c117ca9183ecd22a8fbe9a
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-15 12:01:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -966,13 +966,10 @@ client.on("messageReactionAdd", async (reaction, user) => {
             if(start == 1){
                 let reactionsEmbed = new MessageEmbed()
                 .setTitle('Royal Advisor')
-                .setDescription("[My " + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]")
+                .setDescription("[Good! You're already got the hang of it.]")
                 .setColor('#DC143C')
                 let messageEmbed = await reaction.message.channel.send(reactionsEmbed)
-                //var msgSend = reaction.message.channel.send("Royal Advisor \n [My " + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
-                //let msg = await reaction.message.channel.send(msgSend)
                 messageEmbed.react('➡️')
-                reaction.message.channel.send("Royal Advisor \n [Good! You're already got the hang of it.]");
                 start();
             } //else if(start == 4){
                 //message.channel.send("");
