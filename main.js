65bc4ffa68aabb6e07895e131a25a9fb939d8970
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-15 11:04:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -914,8 +914,8 @@ client.on("messageReactionAdd", async (reaction, user) => {
             if(start == 0){
                 king();
                 startAdd();
-                var msgSend = message.channel.send("Royal Advisor \n [My" + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
-                let msg = await message.channel.send(msgSend)
+                var msgSend = reaction.channel.send("Royal Advisor \n [My" + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
+                let msg = await reaction.channel.send(msgSend)
                 msg.react('✅')
                 msg.react('❌')
             }
@@ -936,8 +936,8 @@ client.on("messageReactionAdd", async (reaction, user) => {
             if(start == 0){
                 queen();
                 startAdd();
-                var msgSend = message.channel.send("Royal Advisor \n [My" + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
-                let msg = await message.channel.send(msgSend)
+                var msgSend = reaction.channel.send("Royal Advisor \n [My" + gender1 + ", I'll help you rule your kingdom. \n but you have to make the tough choices. \n Just say yes or no, understand?]");
+                let msg = await reaction.channel.send(msgSend)
                 msg.react('✅')
                 msg.react('❌')
             }
