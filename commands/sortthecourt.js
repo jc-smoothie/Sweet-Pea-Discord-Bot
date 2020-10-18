790537116fa83480153e74c69e206a90614d5bd3
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-18 11:05:03 -0400

    Delete sortthecourt.js

--- commands/sortthecourt.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'sortthecourt',
-    description: "this is the sourt the court game!",
-    execute(message, args){
-        //message.channel.send("");
-        message.channel.send("======================================== \n .....................................SORT THE COURT..................................... \n <VERSION 1.2.4> \n - NEW CHARACTERS! \n       Dragon, Black Smith, Skelly, Yarno, \n       Button Boy, Sdvisor Pia, \n       King Andromedus, Queen Chanterelle, \n       Queen Cerith, Advisor Pontus, \n       and Advisor Agaric \n - Added achievements! \n - More options with  Royal Advisor \n - True ending: Council of Crowns \n ======================================== \n  \n Are you a king or queen?");
-        //message.author.send("Hey there " + message.author + "!");
-    }
-}
