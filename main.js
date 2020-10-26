bae9596579f0a3cb3b95f76bd020bef1eab13e1d
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-25 20:25:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ console.log(`emitter: ${emitter.getMaxListeners()}`);
 
 client.commands = new Discord.Collection();
 
-client.login('process.env.NzI5MTQyMDczMTI2NjgyNjQ0.XwEoeQ.TuZetTg9OHAwM-JEmGTdOGd4Hv4');
+client.login(process.env.token);
 //client.login('NzQyMTE3MDUxNzYxNjg4NjM3.XzBcXQ.8bnHz6YKfLAYO_Wlk1s-WxV-Gjw');
 
 //Variables
