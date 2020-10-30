3be4aaf2021cba79bd0f66892ebebc7c35d3a5f4
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-30 00:09:37 -0400

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'rank',
-    description: "this assigns the Traveler role!",
+    description: "this assigns server roles!",
     execute(message, args){
         const Asian_Invasion = '637447111725809664';
         const Weeb = '694999716047618199';
@@ -12,28 +12,6 @@ module.exports = {
         const MOD = '727680375266213968';
         const Traveler = '708750286038106133';
         //const Bot = '727682963415760906';
-
-        //Role Variables
-        const Black = '771568348642672670';
-        const Gray = '771568601693159454';
-        const White = '771568680416837642';
-        const Pink = '771568756672692266';
-        const Red = '771568870732857387';
-        const Pastel_Red = '771568989163225120';
-        const Pastel_Orange = '771569098341482497';
-        const Orange = '771569241861652480';
-        const Yellow = '771569332803076101';
-        const Pastel_Yellow = '771569409910374402';
-        const Yellow_Green = '771569562620526592';
-        const Green = '771569741596327946';
-        const Pastel_Green = '771569860014374913';
-        const Light_Blue_Green = '771570030109917214';
-        const Blue_Green = '771570265511428129';
-        const Pastel_Blue = '771570337853997067';
-        const Blue = '771570340559585290';
-        const Indigo = '771570666041769984';
-        const Violet = '771570721524154389';
-        const Pastel_Purple = '771570764938739743';
 
         //if(message.member.roles.some(role => role.name === 'Whatever')) {}
         
