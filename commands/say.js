58f65a1ade1a4a51ffeadbd5588cd6e38c4a90a9
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-30 09:28:41 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'say',
     description: "this says what someone types after the command!",
     execute(message, args){
-        if(message.member.roles.some(role => role.name === 'Staff' || message.member.roles.some(role => role.name === 'Yomifu Staff')) {
+        if(message.member.roles.some(role => role.name === 'Staff') || message.member.roles.some(role => role.name === 'Yomifu Staff')) {
             const msg = message.content.slice(4);
             message.channel.send(msg);
         } else{
