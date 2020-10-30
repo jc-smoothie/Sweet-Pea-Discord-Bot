de3baf355b19e4c3aa61c239e932126b94bf1a5d
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-30 12:43:52 -0400

    Update color.js

--- commands/color.js
+++ commands/color.js
@@ -76,11 +76,11 @@ module.exports = {
             const Pastel_Purple = '';
         }
         
-        if(message.channel.server.id == Asian_Invasion){
+        if(message.channel.guild.id == Asian_Invasion){
             Asian_Invasion_roles();
-        } else if(message.channel.server.id == nico_nico_nii){
+        } else if(message.channel.guild.id == nico_nico_nii){
             nico_nico_nii_roles();
-        } /*else if(message.channel.server.id == B){
+        } /*else if(message.channel.guild.id == B){
             B_roles();
         }*/
         
