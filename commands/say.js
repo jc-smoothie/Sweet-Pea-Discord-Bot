985edc6d8b74ad9b774b429f1bcab5ab6fc683f3
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-30 09:22:05 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,11 @@ module.exports = {
     name: 'say',
     description: "this says what someone types after the command!",
     execute(message, args){
-        const msg = message.content.slice(4);
-        message.channel.send(msg);
+        if(message.member.roles.some(role => role.name === 'Staff' || message.member.roles.some(role => role.name === 'Yomifu Staff')) {
+            const msg = message.content.slice(4);
+            message.channel.send(msg);
+        } else{
+            message.reply("This message is only available to those with the Staff role!");
+        }
     }
 }
