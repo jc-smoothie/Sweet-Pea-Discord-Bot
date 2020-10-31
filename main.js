32b7f50b23efc4a003376e9293f018f6ca5dd930
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-31 13:16:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -353,6 +353,8 @@ client.on('message', message => {
        message.channel.send(wyrEmbed);
    } else if(command == 'say'){
        client.commands.get('say').execute(message, args);
+   } else if(command == 'bal'){
+    client.commands.get('balance').execute(message, args);
    }
 });
 
