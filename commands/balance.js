dc5af4aa6a06168028be9005a85c098a594a8c56
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-31 13:21:58 -0400

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -8,27 +8,37 @@ module.exports.help = {
     aliases: ["p"]
 }*/
 
+/*module.exports = {
+    name: 'ping',
+    description: "this is a ping command!",
+    execute(message, args){
+        message.channel.send('pong!');
+    }
+}*/
+
 const fs = require("fs");
 const money = require("../money.json");
 
 module.exports.run = async (bot, message, args) => {
-    if(!args[0]){
-        var user = message.author;
-    } else{
-        var user = message.mentions.users.first() || bot.users.get(args[0]);
-    }
-
-    if(!money[user.id]){
-        money[user.id] = {
-            name: bot.users.get(user.id).tag,
-            money: 0
+    execute(message, args){
+        if(!args[0]){
+            var user = message.author;
+        } else{
+            var user = message.mentions.users.first() || bot.users.get(args[0]);
         }
-        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-            if(err) console.log(err);
-        });
+    
+        if(!money[user.id]){
+            money[user.id] = {
+                name: bot.users.get(user.id).tag,
+                money: 0
+            }
+            fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+                if(err) console.log(err);
+            });
+        }
+    
+        return message.channel.send(`${bot.users.get(user.id).username} has $${money[user.id].money}.`);
     }
-
-    return message.channel.send(`${bot.users.get(user.id).username} has $${money[user.id].money}.`);
 }
 
 module.exports.help = {
