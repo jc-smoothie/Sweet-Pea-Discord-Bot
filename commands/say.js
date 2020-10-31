fca0277b7be2f1b66430f4b34403605ae35eb9fe
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-31 12:57:40 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'say',
     description: "this says what someone types after the command!",
     execute(message, args){
+        if(msg == '') return;
         const msg = message.content.slice(4);
         message.channel.send(msg);
     }
