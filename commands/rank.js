650d9fec207e578904ccfdef0a0f2aad7e9529f6
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-10-31 11:19:09 -0400

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -3,37 +3,56 @@ module.exports = {
     description: "this assigns server roles!",
     execute(message, args){
         const Asian_Invasion = '637447111725809664';
-        //const Weeb = '694999716047618199';
-        //const Member = '691304349711859792';
-
+        const jc_smoothie_suppoet_server = '755814665111470142';
         const Eclipsys_Contact = '700620542419664968';
-        //const Yomifu_Staff = '727677441597767680';
-        //const MOD = '727680375266213968';
-        //const Traveler = '708750286038106133';
-        //const Bot = '727682963415760906';
-        
-        const jc_smoothie_support_server = '755814665111470142';
-        //const Member = '759550714862764045';
+
+        function Asian_Invasion_roles(){
+            const Weeb = '694999716047618199';
+            const Member = '691304349711859792';
+        }
+
+        function Eclipsys_Contact_roles(){
+            const Yomifu_Staff = '727677441597767680';
+            const MOD = '727680375266213968';
+            const Traveler = '708750286038106133';
+            const Bot = '727682963415760906';
+        }
+
+        function jc_smoothie_support_server_roles(){
+            const Member = '759550714862764045';
+        }
+
+        if(message.channel.guild.id == Asian_Invasion){
+            Asian_Invasion_roles();
+        } else if(message.channel.guild.id == jc_smoothie_suppoet_server){
+            jc_smoothie_support_server_roles();
+        } else if(message.channel.guild.id == Eclipsys_Contact){
+            Eclipsys_Contact_roles();
+        }/* else if(message.channel.guild.id == nico_nico_nii){
+            nico_nico_nii_roles();
+        } else if(message.channel.guild.id == B){
+            B_roles();
+        } else if(message.channel.guild.id == WHS_Anime_Club){
+            WHS_Anime_Club_roles();
+        }*/
 
         //if(message.member.roles.some(role => role.name === 'Whatever')) {}
         
         const parts = message.content.split(' ');
-        const part1 = parts[1];
-        const part2 = parts[2];
-        const part3 = parts[3];
 
         if(parts[1] == 'traveler'){
             if(member.guild.id == Eclipsys_Contact){
                 if(message.member.roles.some(role => role.name === 'Traveler')) {
                     message.reply("You can not stop being a Traveler!");
+                } else{
+                    message.member.roles.add(Traveler);
+                    message.reply('Welcome, Traveler~ Visit #react-role-assign to get language and region roles.');
                 }
-                message.member.roles.add('708750286038106133');
-                message.reply('Welcome, Traveler~ Visit #react-role-assign to get language and region roles.');
             } else{
                 message.reply('Sorry, either the role you entered is not available or does not exist. Please try again.');
             }
         } else if(parts[1] == 'member'){
-            if(message.member.roles.cache.find(r => r.name === "Member")){
+            /*if(message.member.roles.cache.find(r => r.name === "Member")){
                 //message.member.roles.remove(Member);
                 //message.channel.send("❌ " + "<@" + message.author.id + ">" + ", You no longer have the role Weeb.");
                 message.reply("You can not stop being a member! However, if you do want to leave the server, there's nothing stopping you.");
@@ -42,15 +61,15 @@ module.exports = {
                 message.reply('Welcome, new Member~ Visit #react-role-assign to get language and region roles.');
                 //message.channel.send("✅ " + "<@" + message.author.id + ">" + ", Welcome, new member~");
                 //message.reply('Welcome, fellow Weeb~ Visit #react-role-assign to get language and region roles.');
-            }
-            /*
+            }*/
+            
             if(member.guild.id == Asian_Invasion){
                 if(message.member.roles.cache.find(r => r.name === "Member")){
                     //message.member.roles.remove(Member);
                     //message.channel.send("❌ " + "<@" + message.author.id + ">" + ", You no longer have the role Weeb.");
                     message.reply("You can not stop being a member! However, if you do want to leave the server, there's nothing stopping you.");
                 } else{
-                    message.member.roles.add('691304349711859792');
+                    message.member.roles.add(Member);
                     message.reply('Welcome, new Member~ Visit #react-role-assign to get language and region roles.');
                     //message.channel.send("✅ " + "<@" + message.author.id + ">" + ", Welcome, new member~");
                     //message.reply('Welcome, fellow Weeb~ Visit #react-role-assign to get language and region roles.');
@@ -61,7 +80,7 @@ module.exports = {
                     //message.channel.send("❌ " + "<@" + message.author.id + ">" + ", You no longer have the role Weeb.");
                     message.reply("You can not stop being a member! However, if you do want to leave the server, there's nothing stopping you.");
                 } else{
-                    message.member.roles.add('759550714862764045');
+                    message.member.roles.add(Member);
                     message.reply('Welcome, new Member~ Visit #react-role-assign to get language and region roles.');
                     //message.channel.send("✅ " + "<@" + message.author.id + ">" + ", Welcome, new member~");
                     //message.reply('Welcome, fellow Weeb~ Visit #react-role-assign to get language and region roles.');
@@ -69,7 +88,7 @@ module.exports = {
             } else{
                 message.reply('Sorry, either the role you entered is not available or does not exist. Please try again.')
             }
-            */
+            
         } else if(parts[1] == 'weeb'){
             if(member.guild.id == Asian_Invasion){
                 if(message.member.roles.cache.find(r => r.name === "Weeb")){
