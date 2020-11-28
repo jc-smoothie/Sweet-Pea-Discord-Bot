a273bb66f022830929c8dff02545eb54c3dffdbe
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-11-28 00:59:10 -0500

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,5 +1,6 @@
 
 module.exports = (client) => {
+    /*
     const channelId = '775127852458180608';
 
     client.on('guildMemberAdd', (member) => {
@@ -8,7 +9,8 @@ module.exports = (client) => {
         const message = `Please welcome <@${member.id}> to the server!`;
 
         //const channel = member.guild.channels.cache.get(channelId);
-        //const channel = member.channels.find(channel => channel.name === "welcome");
+        const channel = member.channels.find(channel => channel.name === "welcome");
         channel.send(message);
     })
+    */
 }
