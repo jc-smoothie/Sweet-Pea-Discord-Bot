98fb1ca69f0fa84f3f2a061193d792d7793be8e5
Jeffrey Chu <jeffreyc1689@gmail.com>
2020-11-29 09:26:38 -0500

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'mute',
+    description: "This command mutes a member!",
+    execute(message, args){
+        const target = message.mentions.users.first();
+        if(target){
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'Member');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+
+            let memberTarget = message.guild.members.cache.get(target.id);
+
+            memberTarget.roles.remove(mainRole.id);
+            memberTarget.roles.add(muteRole.id);
+            message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+        } else{
+            message.channel.send("Can not find that member!");
+        }
+    }
+}
