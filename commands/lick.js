fc4c89dec430999ffad1fc8ac95a552f3a88aaff
Jeffrey Chu <jeffreyc1689@gmail.com>
2021-09-19 09:23:09 -0400

    Update lick.js

--- commands/lick.js
+++ commands/lick.js
@@ -10,7 +10,7 @@ module.exports = {
             message.channel.send('Missing Arguments!')
             console.log('Missing args')
         } else{
-            console.log('should work.')
+            console.log('lick command activatedd!')
             const image = Math.floor((Math.random() * 2) + 1);
             if(image == 1){
                 var chosenImage = 'https://cdn.weeb.sh/images/H13HS7S6-.gif';
